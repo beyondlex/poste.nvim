03d3625e23975723e256b0c27498cdca664ee844
lex <lex@work.cn>
2026-07-28 11:51:46 +0800

    feat: add PosteProgressBar highlight group

--- lua/poste/dialog.lua
+++ lua/poste/dialog.lua
@@ -8,6 +8,7 @@ local backdrop_win = nil
 
 local function setup_hl()
   vim.api.nvim_set_hl(0, "PosteBackdrop", { bg = 0x000000 })
+  vim.api.nvim_set_hl(0, "PosteProgressBar", { fg = 0x5c6370 })
 end
 setup_hl()
 vim.api.nvim_create_autocmd("ColorScheme", { callback = setup_hl })
